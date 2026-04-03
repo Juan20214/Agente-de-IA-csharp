9ae306e526a3c2e9acbfdaaaf181ddc28aeea287
Juan Manuel <juandx98.arg@gmail.com>
2026-04-02 21:08:30 -0300

    Agregar archivos de proyecto.

---
+++ AgenteIA/Agente.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AgenteIA
+{
+    public class Agente 
+    {
+        private List<string> memoria = new List<string>();
+
+        
+        public string Procesar(string palabra)
+        {
+            palabra = palabra.ToLower();
+
+            memoria.Add(palabra);
+            
+            string respuesta;
+
+            if (palabra == "hola")
+            {
+
+                respuesta = "Hola!";
+           
+            }
+            
+            else if (palabra == "hora")
+            {
+               
+                respuesta = DateTime.Now.ToShortTimeString();
+            
+            }
+            
+            else if (palabra == "fecha")
+            {
+               
+                respuesta = DateTime.Now.ToShortDateString();
+           
+            }
+            
+            else if (palabra == "salir")
+            {
+               
+                respuesta = "salir";
+           
+            }
+           
+            else
+            {
+               
+                respuesta = "No entiendo";
+           
+            }
+
+            memoria.Add("Agente:" + respuesta);
+
+            return respuesta;
+
+
+        }
+
+        public void MostrarMemoria()
+        {
+            Console.WriteLine("\n--- HISTORIAL ---");
+
+            foreach (var item in memoria)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("-----------------\n");
+        }
+
+    }
+}
---
+++ AgenteIA/Program.cs
@@ -0,0 +1,34 @@
+﻿using AgenteIA;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Agente agente = new Agente();
+
+        while (true)
+        {
+            Console.WriteLine("Escribe la palabra al Agente IA");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "memoria")
+            {
+                agente.MostrarMemoria();
+                continue;
+            }
+
+            string respuesta = agente.Procesar(input);
+
+            if (respuesta == "salir")
+                break;
+
+            Console.WriteLine("Agente: " + respuesta);
+        }
+
+
+
+       
+    }
+}
+
